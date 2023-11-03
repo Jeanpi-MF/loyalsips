105618f1b50ea1804391ee6dafae2c8096c49a67
Jean Morin <jean126pi@gmail.com>
2023-11-03 14:58:55 -0500

    chore: initial commit.

---
+++ src/main/java/com/acme/loyalsips/LoyalsipsApplication.java
@@ -0,0 +1,13 @@
+package com.acme.loyalsips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalsipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyalsipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/loyalsips/LoyalsipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.loyalsips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyalsipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
