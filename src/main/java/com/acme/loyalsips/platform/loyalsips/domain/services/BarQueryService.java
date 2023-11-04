97c69935dd647dad91f5c24b3c926fdf25b7a046
IsaacAmp24 <ampudiaflores@gmailcom>
2023-11-04 17:21:46 -0500

    feat(services): BarQueryService added

--- src/main/java/com/acme/loyalsips/platform/loyalsips/domain/services/BarQueryService.java
+++ src/main/java/com/acme/loyalsips/platform/loyalsips/domain/services/BarQueryService.java
@@ -1,4 +1,13 @@
 package com.acme.loyalsips.platform.loyalsips.domain.services;
 
+import com.acme.loyalsips.platform.loyalsips.domain.model.aggregates.Bar;
+import com.acme.loyalsips.platform.loyalsips.domain.model.queries.GetAllBarQuery;
+import com.acme.loyalsips.platform.loyalsips.domain.model.queries.GetBarByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BarQueryService {
+    Optional<Bar> handle(GetBarByIdQuery query);
+    List<Bar> handle(GetAllBarQuery query);
 }
