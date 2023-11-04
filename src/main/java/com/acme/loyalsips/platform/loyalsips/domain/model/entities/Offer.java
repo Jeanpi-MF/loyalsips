67d82237fcfbb61c7ad56ed773ecba0590eb0936
JDiego <JoseHuamani_20@hotmail.com>
2023-11-04 18:43:20 -0500

    feat(entities): Offer added

--- src/main/java/com/acme/loyalsips/platform/loyalsips/domain/model/entities/Offer.java
+++ src/main/java/com/acme/loyalsips/platform/loyalsips/domain/model/entities/Offer.java
@@ -1,4 +1,23 @@
 package com.acme.loyalsips.platform.loyalsips.domain.model.entities;
 
-public class Offer {
+import com.acme.loyalsips.platform.shared.domain.model.entities.AuditableModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+public class Offer extends AuditableModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String contentUrl;
 }
