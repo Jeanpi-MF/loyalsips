480e91fb0fb9b7cea7e43ac52c59286981dbc375
IsaacAmp24 <ampudiaflores@gmailcom>
2023-11-04 17:31:24 -0500

    feat(services): PointQueryService added

--- src/main/java/com/acme/loyalsips/platform/loyalsips/domain/services/PointQueryService.java
+++ src/main/java/com/acme/loyalsips/platform/loyalsips/domain/services/PointQueryService.java
@@ -1,4 +1,14 @@
 package com.acme.loyalsips.platform.loyalsips.domain.services;
 
+import com.acme.loyalsips.platform.loyalsips.domain.model.aggregates.Point;
+import com.acme.loyalsips.platform.loyalsips.domain.model.queries.GetCustomerPointsQuery;
+import com.acme.loyalsips.platform.loyalsips.domain.model.queries.GetPointByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PointQueryService {
+
+    List<Point> handle(GetCustomerPointsQuery query);
+    Optional<Point> handle(GetPointByIdQuery query);
 }
