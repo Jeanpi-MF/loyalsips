d243bd57b2251c295ce5293865e50480c623e02f
Jean Morin <jean126pi@gmail.com>
2023-11-04 15:44:18 -0500

    feat: Update profiles

--- src/main/java/com/acme/loyalsips/platform/profiles/application/internal/commandservices/ProfileCommandServiceImpl.java
+++ src/main/java/com/acme/loyalsips/platform/profiles/application/internal/commandservices/ProfileCommandServiceImpl.java
@@ -1,4 +1,28 @@
 package com.acme.loyalsips.platform.profiles.application.internal.commandservices;
 
-public class ProfileCommandServiceImpl {
+import com.acme.loyalsips.platform.profiles.domain.model.aggregates.Profile;
+import com.acme.loyalsips.platform.profiles.domain.model.commands.CreateProfileCommand;
+import com.acme.loyalsips.platform.profiles.domain.model.valueobjects.EmailAddress;
+import com.acme.loyalsips.platform.profiles.domain.services.ProfileCommandService;
+import com.acme.loyalsips.platform.profiles.infrastructure.persistence.jpa.repositories.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileCommandServiceImpl implements ProfileCommandService {
+    private final ProfileRepository profileRepository;
+
+    public ProfileCommandServiceImpl(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public Long handle(CreateProfileCommand command) {
+        var emailAddress = new EmailAddress(command.email());
+        profileRepository.findByEmail(emailAddress).map(profile -> {
+            throw new IllegalArgumentException("Profile with email "+ command.email() + "already exists");
+        });
+        var profile = new Profile(command.firstName(), command.lastName(), command.email(), command.street(), command.number(), command.city(), command.country());
+        profileRepository.save(profile);
+        return profile.getId();
+    }
 }
